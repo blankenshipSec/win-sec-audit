57b4ccae92967de86d422e7badca30ef6e21418a
jblankenship <joshua@jblankenship.me>
2026-03-02 19:23:00 -0600

    Add starter PowerShell file

---
+++ auditor.ps1
@@ -0,0 +1,16 @@
+<#
+.SYNOPSIS
+    win-sec-audit - A PowerShell security tool that audits Windows systems against hardening best standards
+
+.DESCRIPTION
+    A PowerShell security tool that audits Windows systems against hardening best standards
+
+.AUTHOR
+    Joshua Blankenship (blankenshipSec)
+
+.LINK
+    https://github.com/blankenshipSec/win-sec-audit
+
+.LICENSE
+    MIT
+#>
